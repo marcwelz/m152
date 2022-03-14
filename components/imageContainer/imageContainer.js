31901ad4f499d6db7378c888226b1285220c9204
Milchmaa <marc.welz03@gmail.com>
2022-03-14 07:19:50 +0100

    installed npm install

--- components/imageContainer/imageContainer.js
+++ components/imageContainer/imageContainer.js
@@ -23,15 +23,8 @@ class ImageContainer extends HTMLElement {
         this.textparagraph = this.shadowRoot.querySelector("p")
 
         const imgsrc = this.getAttribute("imgsrc")
-        const isOpen = this.getAttribute("open")
 
         this.textparagraph.innerHTML = imgsrc.replace("images/images/", "");
-
-        if(isOpen) {
-          this.imagesrc.classList.remove("image-container-img")
-          this.imagesrc.classList.add("image-container-img__open")
-        }
-
         this.imagesrc.setAttribute("src", imgsrc)
 
     }
