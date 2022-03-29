86c26c636bad6c0a31f499b9d1249b0bea237e38
Milchmaa <marc.welz03@gmail.com>
2022-03-29 13:40:08 +0200

    checkd in newest changes

--- backend/src/main/java/com/ch/bbw/mw/backend/controller/Controller.java
+++ backend/src/main/java/com/ch/bbw/mw/backend/controller/Controller.java
@@ -12,13 +12,15 @@ import java.util.Collection;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/m152")
+@CrossOrigin()
 public class Controller {
 
     private final Service service;
 
     @GetMapping(value = "/filename/{filename}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Collection<ImageM152> getImageByFilename(@PathVariable String filename) {
-        return service.getImageByFilename(filename);
+    public ImageM152 getImageByFilename(@PathVariable String filename) {
+        Collection<ImageM152> imageM152 = service.getImageByFilename(filename);
+        return imageM152.iterator().next();
     }
 
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -49,5 +51,16 @@ public class Controller {
     @GetMapping(value = "/convert/png/{filename}")
     public void convertPNGtoJPEG(@PathVariable String filename) throws IOException {
         service.convertPNGtoJPEG(filename);
+    }
+
+    @GetMapping(value = "/fileupload/{filename}")
+    public void randomImageUpload(@PathVariable String filename) {
+        Collection<ImageM152> imageM152 = service.getImageByFilename("IMG_00286.JPG");
+        ImageM152 imageM1521_1 = imageM152.iterator().next();
+        imageM152 = null;
+        imageM152 = service.getImageByFilename("IMG_00423.JPG");
+        ImageM152 imageM1521_2 = imageM152.iterator().next();
+        service.addImage(new ImageM152(filename, imageM1521_1.getBase64ImageString()));
+        service.addImage(new ImageM152("bild_25423.jpg", imageM1521_2.getBase64ImageString()));
     }
 }
